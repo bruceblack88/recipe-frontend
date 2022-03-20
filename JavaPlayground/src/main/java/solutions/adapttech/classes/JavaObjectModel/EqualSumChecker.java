f67b6f98c2e93241fdc3ece56bb78d8b2fc202dc
Bruce Black <bruceblack88@gmail.com>
2022-03-20 08:58:41 -0500

    EqualSumChecker complete

---
+++ JavaPlayground/src/main/java/solutions/adapttech/classes/JavaObjectModel/EqualSumChecker.java
@@ -0,0 +1,10 @@
+package solutions.adapttech.classes.JavaObjectModel;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum (int x, int y, int z){
+        if (x + y == z) {
+            return true;
+        }
+        return false;
+    }
+}
