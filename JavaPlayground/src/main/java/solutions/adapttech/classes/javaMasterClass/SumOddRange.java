b7872e3aeb2dae9daae30ae0c20496a71e066ed1
bruceblack88 <bruceblack88@gmail.com>
2022-03-23 10:02:23 -0500

    SumOddRange complete

--- JavaPlayground/src/main/java/solutions/adapttech/classes/javaMasterClass/SumOddRange.java
+++ JavaPlayground/src/main/java/solutions/adapttech/classes/javaMasterClass/SumOddRange.java
@@ -15,23 +15,21 @@ public class SumOddRange {
     public static int sumOdd(int start, int end) {
         int sum = 0;
         ArrayList<Integer> rangeList = new ArrayList<>();
-        if (end <= start || start == 0 || end == 0) {
+        if (end <= start || start <= 0 || end <= 0) {
             return -1;
         } else {
-            for (int i = 0; i < end; i++) {
-                rangeList.add(i);
+
+            for (int i = start; i <= end ; i++) {
+                    rangeList.add(i);
+            }
+
                 for (int j = 0; j < rangeList.size(); j++) {
-                    if (rangeList.indexOf(j) % 2 != 0) {
-                        (j)++;
-                        sum = rangeList.size();
-                        return sum;
-                    } else {
-                        return -1;
+                    if (rangeList.get(j) % 2 == 0){
+                        continue;
                     }
+                    sum += rangeList.get(j);
                 }
             }
+            return sum;
         }
-        return sum;
-    }
-
 }
